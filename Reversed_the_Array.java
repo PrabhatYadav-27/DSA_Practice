e1f337da00d98cc6784fc7977436eb866fe10af3
PrabhatYadav-27 <prabhaty2727@gmail.com>
2024-05-28 15:15:39 +0530

    finish the above question it runs for odd and even array

--- Reversed_the_Array.java
+++ Reversed_the_Array.java
@@ -9,6 +9,20 @@ public class Reversed_the_Array {
         }
 
         //logic
+        int left =0;
+        int right = arr.length-1;
+        int temp ;
+        while(left < right){
+             temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+
+        for(int i =0;i<n;i++){
+            System.out.print(arr[i]+ " ");
+        }
 
     }
 }
